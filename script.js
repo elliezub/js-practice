29710b59eeba9867c2bf9d02dc3e8fdd330492f8
Ellie <112726692+elliezub@users.noreply.github.com>
2023-05-31 22:38:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-function castSpell(spellName, callback) {
-    console.log(`Casting ${spellName}...`);
-    setTimeout(() => { // built-in JavaScript function that schedules a function to be called after a certain amount of time.
-      console.log(`Finished casting ${spellName}!`);
-      callback();
-    }, 2000); //schedules the arrow function to be called after 2000ms (2 secs)
-  }
+function highAndLow(numbers){
+  let nums = numbers.split(' ').map(Number)
   
-  function playSoundEffect() {
-    console.log("Playing sound effect...");
-  }
-  
-  castSpell("Fireball", playSoundEffect);
+  let min = Math.min(...nums)
+  let max = Math.max(...nums)
+  return max + ' ' + min
+}
+
+console.log(highAndLow("5345345 2 3 765"))
